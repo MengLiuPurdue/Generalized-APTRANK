ff72fafbb263cf6d865c56f83eb7f17b79284bc5
Meng Liu <liumeng19952012@outlook.com>
2017-06-12 17:43:29 -0400

    changing representation of training sets, adding seeds position, changing sampling method, changing type 3 and 4 diffusion, adding S-fold cross validation

--- general_APTRANK.jl
+++ general_APTRANK.jl
@@ -3,8 +3,8 @@
 # ei, ej       - an edge list
 # m, n         - the number of rows and edges of the original data set (to ensure
 #                the reconstructed matrix has the same dimension of the original one)
-# train_rows   - the row positions of training data
-# train_cols   - the column positions of training data
+# train_rows   - the row positions of training data, represented in a tuple of arrays
+# train_cols   - the column positions of training data, represented in a tuple of arrays
 # predict_rows - the row positions where you want predictions
 # predict_cols - the column positions where you want predictions
 # K            - the number of terms used in the APTRANK, default is 8
@@ -17,20 +17,25 @@
 # ratio        - the split ratio between fiiting set and validation set, default is 0.8
 # rho          - a parameter used to control the diffusion process, rho > 0, if a
 #                rho <= 0 is given, it will be replaced by the spectral radius
+# seeds        - seed positions, where the diffusion starts, default is the same as
+#
 #
 # Output:
 # Xa            - the predicted matrix, zero rows or columns in the original graph
 #                 will remain zero.
+# alpha         - the final parameters of APTRANK
+# all_alpha     - records of alphas in each independent experiment
 
 using Convex
-using SCS
 using ECOS
 using StatsBase
+using MLBase
 include("splitRT.jl")
 include("manage_procs.jl")
 
 function general_APTRANK(ei,ej,m,n,train_rows,train_cols,predict_rows,predict_cols;
-                         K = 8,S = 5,diff_type = 1,ratio = 0.8,rho = 0.0)
+                         K = 8,S = 5,diff_type = 1,ratio = 0.8,rho = 0.0,seeds = predict_cols,
+                         sampling_type = 1)
 
   nrows = length(predict_rows)
   ncols = length(predict_cols)
@@ -46,37 +51,68 @@ function general_APTRANK(ei,ej,m,n,train_rows,train_cols,predict_rows,predict_co
   end
   np = nprocs()
   all_alpha = zeros(Float64,K-1,S)
-  for s = 1:S
-    @show s
-    Gf,Gv = splitRT(G,ratio)
-    #@show length(train_rows)
-    #@show size(Gf)
-    #@show size(G)
-    Rf = Gf[train_rows,train_cols]
-    Rv = Gv[predict_rows,predict_cols]
-    Gf,Gv = 0,0
-    gc()
-    newG = G
-    newG[train_rows,train_cols] = Rf
-    newG[train_cols,train_rows] = Rf'
-    Rf = 0
-    gc()
-    F = get_diff_matrix(newG,diff_type,rho)
-    @eval @everywhere F = $F
-    newG = 0
-    gc()
-    X0 = zeros(G.m,ncols)
-    for i = 1:ncols
-      X0[predict_cols[i],i] = 1
+  if sampling_type == 2
+    print("start sampling S-fold cross validation\n")
+    folds = Array{Vector{Any}}(length(train_rows))
+    for i = 1:length(train_rows)
+      nr = length(train_rows[i])
+      nc = length(train_cols[i])
+      @time folds[i] = collect(Kfold(nr*nc,S))
     end
-    N = size(X0,2)
+  end
+  for s = 1:S
+    b = []
+    positions = Array(Array{Int64},length(train_rows))
+    Gf = copy(G)
+    if sampling_type == 1
+      for i = 1:length(train_rows)
+        nr = length(train_rows[i])
+        nc = length(train_cols[i])
+        set = round(Int64,linspace(1,nr*nc,nr*nc))
+        pf = sample(set,round(Int64,ratio*nr*nc),replace = false)
+        pv = setdiff(set,pf)
+        training = G[train_rows[i],train_cols[i]] + 1
+        ii,jj,vv = findnz(training)
+        Rf = sparse(ii[pf],jj[pf],vv[pf]-1,nr,nc)
+        Gf[train_rows[i],train_cols[i]] = Rf
+        b = vcat(b,vv[pv]-1)
+        positions[i] = pv
+      end
+    elseif sampling_type == 2
+      print("extract a new fold.\n")
+      for i = 1:length(train_rows)
+        nr = length(train_rows[i])
+        nc = length(train_cols[i])
+        set = round(Int64,linspace(1,nr*nc,nr*nc))
+        pf = set[folds[i][s]]
+        pv = setdiff(set,pf)
+        training = G[train_rows[i],train_cols[i]] + 1
+        ii,jj,vv = findnz(training)
+        Rf = sparse(ii[pf],jj[pf],vv[pf]-1,nr,nc)
+        Gf[train_rows[i],train_cols[i]] = Rf
+        b = vcat(b,vv[pv]-1)
+        positions[i] = pv
+      end
+    else
+      error("Invalid sampling type!")
+    end
+    @show s
+    F = get_diff_matrix(Gf,diff_type,rho)
+    @eval @everywhere F = $F
+    Gf = 0
+    gc()
+    X0 = spzeros(G.m,G.n)
+    for i = 1:length(seeds)
+      X0[seeds[i],seeds[i]] = 1
+    end
+    N = length(seeds)
     bs = ceil(Int64,N/np)
     nblocks = ceil(Int64,N/bs)
 
-    all_ranges = Array(UnitRange{Int64},nblocks)
+    all_ranges = Array(Array{Int64},nblocks)
     for i = 1:nblocks
       start = 1 + (i-1)*bs
-      all_ranges[i] = start:min(start+bs-1,N)
+      all_ranges[i] = seeds[start:min(start+bs-1,N)]
     end
     for i = 1:np
       t =  X0[:,all_ranges[i]]
@@ -84,26 +120,31 @@ function general_APTRANK(ei,ej,m,n,train_rows,train_cols,predict_rows,predict_co
     end
     X0 = 0
     gc()
-    A = zeros(Float64,nrows*ncols,K-1)
+    Arows = zeros(Int64,length(train_rows)+1)
+    for i = 2:(length(train_rows)+1)
+      Arows[i] = Arows[i - 1] + length(positions[i - 1])
+    end
+    A = zeros(Float64,Arows[length(Arows)],K-1)
     #@show "start"
     for k = 1:K
       @show k
       #@show size(X),size(F)
       @time @everywhere X = F * X
       k == 1 && continue
-      Xh = zeros(Rv.m,Rv.n)
+      Xh = zeros(G.m,G.n)
       for i = 1:np
         Xi = getfrom(i,:X)
         #@show size(Xh[:,all_ranges[i]])
         #@show size(Xi[predict_rows,:])
-        @time Xh[:,all_ranges[i]] = Xi[predict_rows,:]
+        @time Xh[:,all_ranges[i]] = Xi
         Xi = 0
         gc()
       end
-      ii,jj,vv = findnz(Xh)
-      @show sum(isnan(vv))
-      rowids = ii + (jj - 1)*(nrows)
-      A[rowids,k-1] = vv
+      for i = 1:length(train_rows)
+        validating = Xh[train_rows[i],train_cols[i]]
+        validating = validating[:]
+        A[(Arows[i]+1):(Arows[i+1]),k-1] = validating[positions[i]]
+      end
       Xh = 0
       gc()
     end
@@ -115,7 +156,6 @@ function general_APTRANK(ei,ej,m,n,train_rows,train_cols,predict_rows,predict_co
     Qa,Ra = qr(A)
     A = 0
     gc()
-    b = reshape(Rv,prod(size(Rv)),1)
     alpha = Variable(size(Ra,2))
     print("start solving Least Sqaure\n")
     @show Ra
@@ -129,22 +169,24 @@ function general_APTRANK(ei,ej,m,n,train_rows,train_cols,predict_rows,predict_co
     b = 0
     gc()
     all_alpha[:,s] = alpha.value
+    @show alpha.value
   end
   alpha = mean(all_alpha,2)
   alpha = alpha / sum(alpha)
   F = get_diff_matrix(G,diff_type,rho)
   @eval @everywhere F = $F
-  X0 = zeros(G.m,ncols)
-  for i = 1:ncols
-    X0[predict_cols[i],i] = 1
+  X0 = zeros(G.m,G.n)
+  for i = 1:length(seeds)
+    X0[seeds[i],seeds[i]] = 1
   end
-  N = size(X0,2)
+  N = length(seeds)
   bs = ceil(Int64,N/np)
   nblocks = ceil(Int64,N/bs)
-  all_ranges = Array(UnitRange{Int64},nblocks)
+
+  all_ranges = Array(Array{Int64},nblocks)
   for i = 1:nblocks
     start = 1 + (i-1)*bs
-    all_ranges[i] = start:min(start+bs-1,N)
+    all_ranges[i] = seeds[start:min(start+bs-1,N)]
   end
   for i = 1:np
     t =  X0[:,all_ranges[i]]
@@ -153,19 +195,23 @@ function general_APTRANK(ei,ej,m,n,train_rows,train_cols,predict_rows,predict_co
   X0 = 0
   gc()
   A = zeros(Float64,nrows*ncols,K-1)
+  #@show "start"
   for k = 1:K
     @show k
+    #@show size(X),size(F)
     @time @everywhere X = F * X
     k == 1 && continue
-    Xh = zeros(nrows,ncols)
+    Xh = zeros(G.m,G.n)
     for i = 1:np
       Xi = getfrom(i,:X)
-      @time Xh[:,all_ranges[i]] = Xi[predict_rows,:]
+      #@show size(Xh[:,all_ranges[i]])
+      #@show size(Xi[predict_rows,:])
+      @time Xh[:,all_ranges[i]] = Xi
       Xi = 0
       gc()
     end
-    ii,jj,vv = findnz(Xh)
-    #@show sum(isnan(vv))
+    ii,jj,vv = findnz(Xh[predict_rows,predict_cols])
+    @show sum(isnan(vv))
     rowids = ii + (jj - 1)*(nrows)
     A[rowids,k-1] = vv
     Xh = 0
@@ -176,7 +222,7 @@ function general_APTRANK(ei,ej,m,n,train_rows,train_cols,predict_rows,predict_co
   Xa = reshape(Xa,nrows,ncols)
   @show alpha
 
-  return Xa,alpha
+  return Xa,alpha,all_alpha
 end
 
 function get_diff_matrix(G,diff_type,rho)
@@ -192,7 +238,7 @@ function get_diff_matrix(G,diff_type,rho)
   D = sparse(diagm(d))
   Dinv = sparse(diagm(dinv))
   Droot = sparse(diagm(sqrt(dinv)))
-  L = D - G
+  L = G - D
   #@show size(G)
   #@show "done"
   if diff_type == 1
@@ -210,7 +256,7 @@ function get_diff_matrix(G,diff_type,rho)
     end
     F = L / rho
   elseif diff_type == 4
-    F = 0.5*(speye(size(G,1)) - Droot * G * Droot)
+    F = -0.5*(speye(size(G,1)) - Droot * G * Droot)
   else
     error("unknown diffusion type")
   end
