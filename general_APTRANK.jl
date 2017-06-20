fccfbfaac6437904879af756daa7838168369f1b
Meng Liu <liumeng19952012@outlook.com>
2017-06-20 17:44:55 -0400

    just to make sure everything stays up to date

--- general_APTRANK.jl
+++ general_APTRANK.jl
@@ -11,7 +11,7 @@
 # S            - the number of independent experiments during training,
 #               default is 5
 # diff_type    - choose what kind of diffusion matrix you want, 1 for G/rho, 2 for
-#               G*D^{-1}, 3 for (D - G)/rho and 4 for (I - D^{-1/2}*G*D^{-1/2}),
+#               G*D^{-1}, 3 for (D - G)/rho and 4 for (I - D^{-1/2}*G*D^{-1/2})/norm(L,1),
 #               where rho is the spectral radius and D is the out-degree of each
 #               vertex, defalut is 1
 # ratio        - the split ratio between fiiting set and validation set, default is 0.8
@@ -257,7 +257,9 @@ function get_diff_matrix(G,diff_type,rho)
     end
     F = L / rho
   elseif diff_type == 4
-    F = -0.5*(speye(size(G,1)) - Droot * G * Droot)
+    F = -1*(speye(size(G,1)) - Droot * G * Droot)
+    mu = 1/norm(F,1)
+    F = mu*F
   else
     error("unknown diffusion type")
   end
