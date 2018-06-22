56c879c71c18df0d311e453da740bb2a4d03dc75
Huda Nassar <hnassar@purdue.edu>
2018-06-22 12:03:17 -0400

    use the _eff version

--- include_all.jl
+++ include_all.jl
@@ -2,6 +2,6 @@ using SCS
 using ECOS
 include(joinpath(Pkg.dir(),"handy_julia","handy_julia.jl"))
 include("readSMAT.jl")
-include("general_APTRANK.jl")
+include("general_APTRANK_eff.jl")
 include("calc_AUC_new.jl")
 include("splitRT.jl")
